e93eb4c72c0329b0b219eaaca9e5b734b3517d3e
Wandumuzi Mathunjwa <Wandumuzi.Mathunjwa@platinumlife.co.za>
2020-02-25 11:30:54 +0200

    added line chart, fixed css for line chart

--- src/app/charts/line-chart/line-chart.component.ts
+++ src/app/charts/line-chart/line-chart.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { LINE_CHART_COLORS } from '../../shared/chart.color'
+import { ChartDataSets } from 'chart.js';
+import { Label, Color } from 'ng2-charts';
+
+const LINE_CHART_SAMPLE_DATA: any[] = [
+  {data: [12, 34, 56, 78, 90, 120], label: 'Sentiment Analysis'},
+  {data: [23, 34, 45, 56, 67, 78], label: 'Image Recognition'},
+  {data: [21, 35, 12, 45, 67, 99], label: 'Forecasting'}
+];
+
+const LINE_CHART_LABELS: string[] = [
+  'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'
+];
 
 @Component({
   selector: 'app-line-chart',
@@ -6,6 +19,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./line-chart.component.css']
 })
 export class LineChartComponent implements OnInit {
+
+  public lineChartData: ChartDataSets[] =  LINE_CHART_SAMPLE_DATA;
+  public lineChartLabels: Label[] = LINE_CHART_LABELS;
+  lineChartOptions: any = {
+    responsive: true,
+    maintainAspectRatio: false
+  };
+  lineChartLegend: true;
+  lineChartType = 'line';
+  lineChartColors: Color[] = LINE_CHART_COLORS;
 
   constructor() { }
 
