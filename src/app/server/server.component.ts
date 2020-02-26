051e7d508685bba2fb18eb423ae9b6bca12e3373
Wandumuzi Mathunjwa <Wandumuzi.Mathunjwa@platinumlife.co.za>
2020-02-26 13:10:47 +0200

    Updated server component

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -9,10 +9,30 @@ import { Server } from '../shared/server';
 export class ServerComponent implements OnInit {
 
   @Input() serverInput: Server;
+  color: string;
+  buttonText: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.setServerStatus(this.serverInput.isOnline);
+  }
+
+  toggleStatus(onlineStatus: boolean) {
+    console.log(this.serverInput.name + ':', onlineStatus);
+    this.setServerStatus(!onlineStatus);
+  }
+
+  setServerStatus(isOnline: boolean) {
+    if (isOnline) {
+      this.serverInput.isOnline = true;
+      this.color = '#66bb6a';
+      this.buttonText = 'Shut Down';
+    } else { 
+      this.serverInput.isOnline = false;
+      this.color = '#ff6b6b';
+      this.buttonText = 'Turn On';
+    }
   }
 
 }
