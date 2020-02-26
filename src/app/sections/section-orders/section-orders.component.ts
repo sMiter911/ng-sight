cf1ac67b88ffe09447d165240c91fe6b32f4dee2
Wandumuzi Mathunjwa <Wandumuzi.Mathunjwa@platinumlife.co.za>
2020-02-26 12:19:06 +0200

    added sales table with static data

--- src/app/sections/section-orders/section-orders.component.ts
+++ src/app/sections/section-orders/section-orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from 'src/app/shared/order';
 
 @Component({
   selector: 'app-section-orders',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./section-orders.component.css']
 })
 export class SectionOrdersComponent implements OnInit {
+  orders: Order[] = [
+    {id: 1, customer: {id: 1, name: 'Wandu', email: 'd_wandz@yahoo.com', province: 'Gauteng'},
+    total: 200, placed: new Date(2020, 2, 25), fulfilled: new Date(2020, 2, 26)},
+    {id: 2, customer: {id: 1, name: 'Wandu', email: 'd_wandz@yahoo.com', province: 'Gauteng'},
+    total: 200, placed: new Date(2020, 2, 25), fulfilled: new Date(2020, 2, 26)},
+    {id: 3, customer: {id: 1, name: 'Wandu', email: 'd_wandz@yahoo.com', province: 'Gauteng'},
+    total: 200, placed: new Date(2020, 2, 25), fulfilled: new Date(2020, 2, 26)},
+    {id: 4, customer: {id: 1, name: 'Wandu', email: 'd_wandz@yahoo.com', province: 'Gauteng'},
+    total: 200, placed: new Date(2020, 2, 25), fulfilled: new Date(2020, 2, 26)},
+    {id: 5, customer: {id: 1, name: 'Wandu', email: 'd_wandz@yahoo.com', province: 'Gauteng'},
+    total: 200, placed: new Date(2020, 2, 25), fulfilled: new Date(2020, 2, 26)}
+  ];
 
   constructor() { }
 
